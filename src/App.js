47aad19efa5770137e84f3585c0718f70b29c6c1
AayushKumar176 <aayushdbg2002@gmail.com>
2023-07-03 20:04:46 +0530

    added env

--- src/App.js
+++ src/App.js
@@ -11,8 +11,7 @@ import LoadingBar from 'react-top-loading-bar'
 
 const App =()=> {
   const pageSize=6;
-  // const apiKey=process.env.REACT_APP_NEWS_API
-  const apiKey="49ad8ef79b1141cb8810a45224e34566"
+  const apiKey=process.env.REACT_APP_NEWS_API
   const [progress, setProgress] = useState(0)
   // state={
   //   progress:0
